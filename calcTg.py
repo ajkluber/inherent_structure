f5e9fee131e34ff0b04ebeec42bce4a13f193b49
Alexander Kluber <ajkluber@rice.edu>
2016-02-02 16:35:44 -0600

    Plot energy, entropy in dimensionless units

--- calcTg.py
+++ calcTg.py
@@ -45,7 +45,7 @@ def get_energies(temps):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="inherent structure analysis to get Tg")
-    parser.add_argument("--nodisplay",
+    parser.add_argument("--display",
                         action='store_true',
                         help="Don't display plot.")
 
@@ -55,7 +55,7 @@ if __name__ == "__main__":
                         help="Number of bins.")
 
     args = parser.parse_args()
-    nodisplay = args.nodisplay
+    display = args.display
     nbins = args.n_bins
 
     temps = [ x.rstrip("\n") for x in open("ticatemps", "r").readlines() ]
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     SconfE_U = np.log(omegaE_U)
 
     # REM fit to the configurational entropy yields Tg!
-    coeff = np.polyfit(mid_bin_U, SconfE_U, 2)
+    coeff = np.polyfit(beta*mid_bin_U, SconfE_U, 2)
     a, b, c = coeff
     E_GS = (-b + np.sqrt(b*b - 4.*a*c))/(2.*a)
     SconfE_U_interp = np.poly1d(coeff)
@@ -116,29 +116,40 @@ if __name__ == "__main__":
     np.savetxt("Sconf_Enn.dat", SconfE_U)
 
     # solve for other REM parameters using fit.
-    Efit = np.linspace(0, max(mid_bin), 1000)
+    Efit = np.linspace(0, beta*max(mid_bin), 1000)
     Tk_line = lambda E: dSdE(E_GS)*E - dSdE(E_GS)*E_GS
     E_Tk_line = np.linspace(0.95*E_GS, 1.01*E_GS, 1000)
 
     # Plot
     plt.figure()
-    plt.plot(mid_bin, beta*mid_bin, label="$E$")
-    plt.plot(mid_bin, SconfE, label="$S_{conf}$")
-    plt.plot(mid_bin_U, SconfE_U, label="$S_{conf}(Q_u)$")
+    plt.plot(beta*mid_bin, beta*mid_bin, label="$E$")
+    plt.plot(beta*mid_bin, SconfE, label="$S_{conf}$")
+    plt.plot(beta*mid_bin_U, SconfE_U, label="$S_{conf}(Q_u)$")
     plt.plot(Efit, SconfE_U_interp(Efit), ls='--', lw=1, label="REM fit")
+    ax = plt.gca()
+    for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(18)
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label.set_fontsize(18)
     
     #plt.plot(E_Tk_line, Tk_line(E_Tk_line), 'k')
-    #plt.annotate('$\\frac{1}{T_k} = \\frac{\\partial S }{\\partial E}$', xy=(E_GS, 0),  xycoords='data',
-    #        xytext=(0.4, 0.2), textcoords='axes fraction', fontsize=22,
-    #        arrowprops=dict(facecolor='black', shrink=0.1))
-    plt.ylabel("Entropy $S_{conf}$ ($k_B$)")
-    plt.xlabel("Energy ($k_B T$)")
-    plt.title("$T_f = {:.2f}$  $T_k = {:.2f}$".format(T,Tg))
-    plt.legend(loc=2)
-    plt.ylim(0, np.max(SconfE))
+    plt.annotate('$\\frac{1}{T_g} = \\frac{\\partial S }{\\partial E}$   ', xy=(E_GS, 0),  xycoords='data',
+            xytext=(0.4, 0.2), textcoords='axes fraction', fontsize=30,
+            arrowprops=dict(facecolor='gray', shrink=0.01))
+    plt.annotate("$T_f = {:.2f} K$\n$T_g = {:.2f} K$".format(T,Tg), xy=(0,0), xycoords="axes fraction",
+            xytext=(0.4, 0.8), textcoords='axes fraction', fontsize=22)
+    plt.ylabel("Entropy $S_{conf}$ ($k_B$)", fontsize=25)
+    plt.xlabel("Energy ($k_B T$)", fontsize=25)
+    #plt.title("$T_f = {:.2f}$  $T_g = {:.2f}$".format(T,Tg))
+    #plt.title("High frustration   $b = 1.00$", fontsize=25)
+    #plt.title("Low frustration   $b = 0.1$", fontsize=25)
+    plt.legend(loc=2, fontsize=18)
+    plt.xlim(0, beta*np.max(mid_bin))
+    #plt.ylim(0, np.max(SconfE))
+    plt.ylim(0, beta*np.max(mid_bin))
     plt.savefig("REM_fit_of_dos.png", bbox_inches="tight") 
     plt.savefig("REM_fit_of_dos.pdf", bbox_inches="tight") 
-    if nodisplay is not None:
+    if display:
         plt.show()
 
     os.chdir("..")
