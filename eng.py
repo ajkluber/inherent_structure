30a2fb4f5ae017ea552be44b55bc40fb5631bb98
Alexander Kluber <ajkluber@rice.edu>
2016-07-25 16:57:36 -0500

    [eng] calculate backbone, native, nonative energy terms

--- eng.py
+++ eng.py
@@ -1,24 +1,44 @@
 import os
-import glob
+import time
+import argparse
+import numpy as np
 
-from simulation.calc.scripts.Enat import calc_Enat_for_directories 
-from simulation.calc.scripts.Enn import calc_Enn_for_directories
+import mdtraj as md
+
+import model_builder as mdb
 
 if __name__ == "__main__":
-    #import time
-    #starttime = time.time()
+    parser = argparse.ArgumentParser(description="Energy minimization for inherent structure analysis.")
+    parser.add_argument("path_to_ini", help="Path to .ini file.")
+    parser.add_argument("n_native_pairs", type=int, help="Number of native pairs.")
+    parser.add_argument("--trajfile", default="traj.xtc", help="Trajectory filename.")
+    parser.add_argument("--topfile", default="ref.pdb", help="Path to topology file.")
 
-    temps = [ x.rstrip("\n") for x in open("ticatemps", "r").readlines() ]
-    path_to_params = "."
+    args = parser.parse_args()
+    path_to_ini = args.path_to_ini
+    n_native_pairs = args.n_native_pairs
+    topfile = args.topfile
+    trajfile = args.trajfile
 
-    for i in range(len(temps)): 
-        os.chdir("{}/inherent_structures".format(temps[i]))
-        trajfiles = glob.glob("rank_*/all_frames.xtc")
-        if trajfiles == []:
-            print "no output trajs found!"
-        else:
-            calc_Enat_for_directories(trajfiles, path_to_params=path_to_params)
-            calc_Enn_for_directories(trajfiles, path_to_params=path_to_params)
+    starttime = time.time()
+
+    model, fitopts = mdb.inputs.load_model(path_to_ini)
+
+    with open("Qtanh_0_05_profile/T_used.dat", "r") as fhandle:
+        T_used = float(fhandle.read())
+
+    tempdirs = [ "T_{:.2f}_{}".format(T_used, x) for x in [1,2,3] ]
+
+    for i in range(len(tempdirs)):
+        os.chdir(tempdirs[i] + "/inherent_structures")
+        traj = md.load(trajfile, top=topfile)
+        Ebackbone = model.Hamiltonian.calc_bond_energy(traj) + \
+                    model.Hamiltonian.calc_angle_energy(traj) + \
+                    model.Hamiltonian.calc_dihedral_energy(traj) 
+        Enat, Enon = model.Hamiltonian.calc_native_nonative_pair_energy(traj, n_native_pairs)
+        np.save("Ebackbone.npy", Ebackbone)
+        np.save("Enat.npy", Enat)
+        np.save("Enon.npy", Enon)
         os.chdir("../..")
 
-    #print "{} min".format((time.time() - starttime)/60.)
+    print "calculation took: {:.4} min".format((time.time() - starttime)/60.)
